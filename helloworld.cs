2d9a3070cf7a48b673c172c68de354fa7c2f7d45
L0Ldev <77333759+L0Ldev@users.noreply.github.com>
2022-12-15 13:24:41 +0200

    C#

--- helloworld.cs
+++ helloworld.cs
@@ -1,7 +1,7 @@
 namespace HelloWorld {
     class Hello {         
         static void Main(string[] args) {
-            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello world!");
         }
     }
 }
